a9abc741c90aa97f56cb680e03982fc9811111f0
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-05 15:13:15 +0530

    Initial Commit

---
+++ Basic-Card-Game/Basic_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_Card_GameApp.swift
+//  Basic-Card-Game
+//
+//  Created by Meet Balani on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Basic_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
