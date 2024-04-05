00f3960620de065f084003f20a80953167475a10
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-05 15:48:25 +0530

    Created UI

--- Basic-Card-Game/ContentView.swift
+++ Basic-Card-Game/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.backgroundCloth).resizable().ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image(.logo)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(.card2)
+                    Spacer()
+                    Image(.card7)
+                    Spacer()
+                }
+                Spacer()
+                Image(.button)
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player").font(.headline).padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU").font(.headline).padding(.bottom, 10.0)
+                        Text("0").font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
