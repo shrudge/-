e1bdfc76c21d0e0d0a9313f7285cc5be7f77fe4a
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-07 11:56:47 +0530

    Added logic

--- Basic-Card-Game/ContentView.swift
+++ Basic-Card-Game/ContentView.swift
@@ -8,39 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var player_card = "card7"
+    @State var player_score =  0
+    @State var cpu_card = "card5"
+    @State var cpu_score = 0
+    
     var body: some View {
         ZStack {
-            Image(.backgroundCloth).resizable().ignoresSafeArea()
+            Image(.backgroundPlain)
+                .resizable()
+                .ignoresSafeArea()
             VStack {
                 Spacer()
                 Image(.logo)
                 Spacer()
                 HStack {
                     Spacer()
-                    Image(.card2)
+                    Image(player_card)
                     Spacer()
-                    Image(.card7)
+                    Image(cpu_card)
                     Spacer()
                 }
                 Spacer()
-                Image(.button)
+            
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+
                 Spacer()
                 HStack {
                     Spacer()
                     VStack {
-                        Text("Player").font(.headline).padding(.bottom, 10.0)
-                        Text("0").font(.largeTitle)
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(player_score))
+                            .font(.largeTitle)
                     }
                     Spacer()
                     VStack {
-                        Text("CPU").font(.headline).padding(.bottom, 10.0)
-                        Text("0").font(.largeTitle)
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpu_score))
+                            .font(.largeTitle)
                     }
                     Spacer()
                 }
                 .foregroundColor(.white)
                 Spacer()
             }
+        }
+    }
+    func deal() {
+        //Giving Random values to the cards
+        
+        //player card
+        let player_card_value = Int.random(in: 2...14)
+        player_card = "card" + String(player_card_value)
+        
+        //CPU card
+        let cpu_card_value = Int.random(in: 2...14)
+        cpu_card = "card" + String(cpu_card_value)
+        
+        //Score updation function that checks the condition
+        if player_card_value > cpu_card_value {
+            player_score += 1
+        }
+        else if cpu_card_value > player_card_value {
+            cpu_score += 1
         }
     }
 }
